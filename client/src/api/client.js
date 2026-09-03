a7dcb306d2d9fb2402064d2fbd7bccb8a8d7cb64
Deepak Kumar Singh <rishusingh13615@gmail.com>
2026-09-03 18:41:28 +0530

    fix frontend for vercel

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,4 +1,4 @@
-const BASE_URL ="https://server-three-xi-78.vercel.app/";
+const BASE_URL ="https://server-three-xi-78.vercel.app/api";
  
 async function request(path, options = {}) {
   const res = await fetch(`${BASE_URL}${path}`, {
